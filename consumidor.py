7dc7eaeb1720f037ea69f67d38f3a92b86bd1969
rujhfd96 <rujhfd96@gmail.com>
2023-12-03 15:15:28 -0300

    Atualiza códigos para suportar conversão de Dólares e Euros

--- consumidor.py
+++ consumidor.py
@@ -1,14 +1,19 @@
 import requests
 
-url_provedor = 'URL_DO_WEBSERVICE_PROVEDOR/converter'
+url_provedor = 'http://127.0.0.1:5000/converter'
 
-data = {
-    'valor': 50.00,
-    'moeda': 'Dólar'
-}
+# Solicitamos ao usuário os detalhes da conversão
+valor = float(input('Valor a converter: '))
+moeda = input('Moeda a converter (Dólar ou Euro): ')
 
+# Enviando a solicitação para o provedor
+data = {'valor': valor, 'moeda': moeda}
 response = requests.post(url_provedor, json=data)
 
-resultado = response.json()
-
-print(f"Valor convertido: {resultado['valor_convertido']} {resultado['moeda_destino']}")
+# Exibindo o resultado
+if response.status_code == 200:
+    resultado = response.json()
+    valor_convertido = resultado['valor_convertido']
+    print(f'Valor convertido: {valor_convertido:.2f} Real')
+else:
+    print(f'Erro na solicitação: {response.status_code}')
--- provedor.py
+++ provedor.py
@@ -11,7 +11,14 @@ def converter():
 
     # Lógica para converter moeda (exemplo: fixar valor da cotação)
     cotacao_dolar = 5.2
-    valor_convertido = valor * cotacao_dolar
+    cotacao_euro = 6.2
+
+    if moeda == 'Dólar':
+        valor_convertido = valor * cotacao_dolar
+    elif moeda == 'Euro':
+        valor_convertido = valor * cotacao_euro
+    else:
+        return jsonify({'erro': 'Moeda não suportada'}), 400
 
     return jsonify({'valor_convertido': valor_convertido, 'moeda_destino': 'Real'})
 
