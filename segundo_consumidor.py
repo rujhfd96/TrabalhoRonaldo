30c05932a8e1883106aa233aee714bbdb3dcc3aa
rujhfd96 <rujhfd96@gmail.com>
2023-12-04 22:11:37 -0300

    Descrição do commit

--- segundo_consumidor.py
+++ segundo_consumidor.py
@@ -1,8 +1,6 @@
-
-
 import requests
 
-url_provedor2 = 'http://127.0.0.1:5000/converter2'
+url_provedor2 = 'http://127.0.0.1:5001/converter2'
 
 valor = float(input('Valor a converter: '))
 moeda_origem = input('Moeda de origem (Dólar ou Euro): ')
@@ -14,8 +12,12 @@ if response.status_code == 200:
     resultado = response.json()
     valor_convertido_bitcoin = resultado['valor_convertido_bitcoin']
     valor_convertido_ethereum = resultado['valor_convertido_ethereum']
+    valor_convertido_litecoin = resultado['valor_convertido_litecoin']
+    valor_convertido_ripple = resultado['valor_convertido_ripple']
 
     print(f'Valor convertido para Bitcoin: {valor_convertido_bitcoin:.8f} BTC')
     print(f'Valor convertido para Ethereum: {valor_convertido_ethereum:.6f} ETH')
+    print(f'Valor convertido para Litecoin: {valor_convertido_litecoin:.6f} LTC')
+    print(f'Valor convertido para Ripple: {valor_convertido_ripple:.6f} XRP')
 else:
     print(f'Erro na solicitação: {response.status_code}')
